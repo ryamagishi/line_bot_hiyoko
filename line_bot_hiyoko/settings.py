5b3e62dc5ee7fa7ecc6939668651f55875a96814
Ryota Yamagishi <ddzsryotag@gmail.com>
2018-10-31 07:40:24 +0900

    heroku setting

--- line_bot_hiyoko/settings.py
+++ line_bot_hiyoko/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3l-bc@2p=63hffs6-u2^u47l9&7e#n6nq9igl8485nq*h8=8rq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
