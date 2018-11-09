532d67f8b9ef2a36ce394ba0e7a3cb202036aa75
Ryota Yamagishi <ddzsryotag@gmail.com>
2018-11-09 11:54:46 +0900

    settng tmp cors

--- line_bot_hiyoko/settings.py
+++ line_bot_hiyoko/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
 ]
 LINE_BOT_HIYOKO_APPS = [
     'line_api',
@@ -55,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'line_bot_hiyoko.urls'
@@ -118,3 +120,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# todo setting cors
+CORS_ORIGIN_ALLOW_ALL = True
